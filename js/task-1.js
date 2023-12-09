29a34f2ccc7936fed1123782eaae5712fea30fb4
ddooss81 <144255208+ddooss81@users.noreply.github.com>
2023-12-09 16:47:00 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,9 @@
-'use strict';
-
+const start = 6;
+const end = 17;
+let number = start+1;
+for (number; number < end; number++) {
+  if (number % 5 === 0) {
+    break;
+  }
+}
+console.log(number);
